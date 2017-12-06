c8e9fba6ddefdc161287999021ba609aa0baedaf
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2017-12-06 23:05:14 +0000

    added rendered pages as well as posts and redirects

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+const shortId = require("shortid");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -8,17 +12,31 @@ var urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  if (longURL === undefined) {
+    res.sendStatus(404);
+    return;
+  }
+  res.redirect(longURL);
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   var shortURL = req.params.id;
   var longURL = urlDatabase[shortURL];
   var templateVars = { shortUrl: shortURL, longUrl: longURL };
   res.render("urls_show", templateVars);
 });
+
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
@@ -28,6 +46,28 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls", (req, res) => {
+  console.log(shortId.generate());
+  console.log(req.body.longURL);
+  var newId = shortId.generate();
+  var longUrl = req.body.longURL;
+  // debug statement to see POST parameters
+  urlDatabase[newId] = longUrl;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${newId}`); // Respond with 'Ok' (we will replace this)
+});
+
+// function generateRandomString(string) {
+//   let possible = "1234567890";
+//   for (var i = 0; i < 26; i++) {
+//     possible += String.fromCharCode(65 + i) + String.fromCharCode(97 + i);
+//   }
+
+//   console.log(possible);
+// }
+
+// generateRandomString();
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
