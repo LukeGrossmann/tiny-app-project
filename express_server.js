e75d487a409c3ac79b0a18262061c6a4d7eb2423
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2017-12-10 20:25:34 +0000

    added time/visits feature, modified redirects to reflect projects standards, fixed bugs with login and register to return proper error messages,  re-formatted code for better readability

--- express_server.js
+++ express_server.js
@@ -3,25 +3,34 @@ const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 const shortId = require("shortid");
-const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const bcrypt = require("bcrypt");
+const cookieSession = require("cookie-session");
 
 app.use(morgan("tiny"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["verysecretkey"]
+  })
+);
 
 app.set("view engine", "ejs");
 
 var urlDatabase = {
   b2xVn2: {
     longUrl: "http://www.lighthouselabs.ca",
-    userId: "hunter"
+    userId: "hunter",
+    date: "fake",
+    visits: 3
   },
   "9sm5xK": {
     longUrl: "http://www.google.com",
-    userId: "titan"
+    userId: "titan",
+    date: "also fake",
+    visits: 100
   }
 };
 
@@ -49,16 +58,25 @@ var users = {
 };
 
 function is_duplicate_email(email) {
+  console.log(email);
+  var contains = false;
   for (var prop in users) {
     console.log("checking email for", prop);
-    return users[prop].email === email;
+    console.log(users[prop].email);
+    contains = contains || users[prop].email === email;
+    console.log(contains);
   }
+  return contains;
+}
+
+function belongsTo(user_id, url) {
+  return urlDatabase[url].userId === user_id;
 }
 
 function urlsForUser(user_id) {
   var answer = {};
   for (var shortURL in urlDatabase) {
-    if (urlDatabase[shortURL].userId === user_id) {
+    if (belongsTo(user_id, shortURL)) {
       answer[shortURL] = urlDatabase[shortURL];
     }
   }
@@ -74,11 +92,6 @@ app.post("/register", (req, res) => {
   var email = req.body["email"];
   var password = bcrypt.hashSync(req.body["password"], 10);
 
-  users[newId] = {
-    id: newId,
-    email: email,
-    password: password
-  };
   console.log(users);
   if (email === "" || password === "") {
     res.sendStatus(400);
@@ -88,7 +101,13 @@ app.post("/register", (req, res) => {
     return;
   }
 
-  res.cookie("user_id", newId);
+  users[newId] = {
+    id: newId,
+    email: email,
+    password: password
+  };
+
+  req.session.user_id = newId;
 
   res.redirect("/urls");
 });
@@ -103,6 +122,7 @@ app.post("/login", (req, res) => {
   var email = req.body["email"];
   var password = req.body["password"];
   var user;
+  console.log(users);
   for (var userId in users) {
     if (users[userId].email === email) {
       user = users[userId];
@@ -117,19 +137,23 @@ app.post("/login", (req, res) => {
     return;
   }
 
-  res.cookie("user_id", user.id);
+  req.session.user_id = user.id;
 
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
   // const { username } = req.body;
-  res.clearCookie("user_id");
+  req.session.user_id = null;
   res.redirect("/urls");
 });
 
 app.get("/", (req, res) => {
-  res.redirect("/urls");
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.get("/urls.json", (req, res) => {
@@ -138,19 +162,22 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls", (req, res) => {
   // looping over userUrls in order to just display the users urls on the /urls page.
-  var userUrls = urlsForUser(req.cookies["user_id"]);
+  var userUrls = urlsForUser(req.session.user_id);
   let templateVars = {
     urls: userUrls,
-    user: users[req.cookies["user_id"]]
+    user: users[req.session.user_id]
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
+  if (!req.session.user_id) {
+    res.redirect("/login");
+  }
   var templateVars = {
-    user: users[req.cookies.user_id]
+    user: users[req.session.user_id]
   };
-  if (!users[req.cookies.user_id]) {
+  if (!users[req.session.user_id]) {
     res.redirect("/login");
     return;
   }
@@ -162,7 +189,9 @@ app.post("/urls", (req, res) => {
   var objectUrl = req.body.longURL;
   urlDatabase[newId] = {
     longUrl: objectUrl,
-    userId: req.cookies.user_id
+    userId: req.session.user_id,
+    date: Date().toString(),
+    visits: 0
   };
   res.redirect(`/urls/${newId}`);
 });
@@ -170,14 +199,28 @@ app.post("/urls", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   var shortURL = req.params.id;
   //need to change
-  var objectUrl = urlDatabase[shortURL].longUrl;
+  var objectUrl = undefined;
+  if (shortURL in urlDatabase) {
+    objectUrl = urlDatabase[shortURL].longUrl;
+    urlDatabase[shortURL].visits++;
+  }
+  //   shortURL in urlDatabase ? urlDatabase[shortURL].longUrl : undefined;
+  // }
+
+  // var a = condition ? true case : false case
+  // a = true ? "it is true" : "it is false";
+  // a === true
+  // a = false ? "it is true" : "it is false";
+  // a === false
+
   var templateVars = {
     shortUrl: shortURL,
     longUrl: objectUrl,
-    user: users[req.cookies["user_id"]]
+    date: urlDatabase[shortURL] ? urlDatabase[shortURL].date : "",
+    visits: urlDatabase[shortURL] ? urlDatabase[shortURL].visits : "",
+    user: users[req.session.user_id],
+    owned: objectUrl ? belongsTo(req.session.user_id, shortURL) : false
   };
-  console.log("Logged in page");
-  console.log(templateVars);
   res.render("urls_show", templateVars);
 });
 
@@ -186,8 +229,8 @@ app.post("/urls/:id/update", (req, res) => {
 
   console.log(shortId);
   console.log(req.body.longUrl);
-  console.log("cookies", req.cookies);
-  let curUserID = req.cookies.user_id;
+  console.log("cookies", req.session);
+  let curUserID = req.session.user_id;
   if (!(urlDatabase[shortId]["userId"] === curUserID)) {
     res.send("<h1>wrong </h1>");
     return;
@@ -199,7 +242,7 @@ app.post("/urls/:id/update", (req, res) => {
 
 app.post("/urls/:id/delete", (req, res) => {
   var shortId = req.params.id;
-  let curUserID = req.cookies.user_id;
+  let curUserID = req.session.user_id;
   if (!(urlDatabase[shortId]["userId"] === curUserID)) {
     // res.sendStatus(403);
     res.send("<h1>You can not delete what isn't yours </h1>");
